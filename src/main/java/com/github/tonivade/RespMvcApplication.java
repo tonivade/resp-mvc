7a9b1fb750a949098267744d369e6fe67979fa8b
Antonio Muñoz <antoniogmc@gmail.com>
2016-12-13 22:13:35 +0100

    initial commit

---
+++ src/main/java/com/github/tonivade/RespMvcApplication.java
@@ -0,0 +1,12 @@
+package com.github.tonivade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespMvcApplication.class, args);
+	}
+}
