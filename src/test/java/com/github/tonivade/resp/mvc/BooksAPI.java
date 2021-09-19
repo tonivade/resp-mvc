f99733b9e23f769170cf2dd08dbb919b43aeb4d8
Antonio Muñoz <antoniogmc@gmail.com>
2021-09-19 22:49:18 +0200

    updated

--- src/test/java/com/github/tonivade/resp/mvc/BooksAPI.java
+++ src/test/java/com/github/tonivade/resp/mvc/BooksAPI.java
@@ -13,13 +13,17 @@ import static com.github.tonivade.zeromock.api.Handlers.ok;
 import static com.github.tonivade.zeromock.api.Headers.contentJson;
 import static com.github.tonivade.zeromock.api.Serializers.objectToJson;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 import com.github.tonivade.purefun.Consumer1;
 import com.github.tonivade.purefun.Function1;
 import com.github.tonivade.purefun.Function2;
 import com.github.tonivade.purefun.Producer;
+import com.github.tonivade.purefun.type.Try;
+import com.github.tonivade.purejson.TypeToken;
 import com.github.tonivade.resp.mvc.BooksService.Book;
+import com.github.tonivade.zeromock.api.Bytes;
 import com.github.tonivade.zeromock.api.HttpRequest;
 import com.github.tonivade.zeromock.api.RequestHandler;
 
@@ -33,25 +37,26 @@ public class BooksAPI {
 
   public RequestHandler findAll() {
     Producer<List<Book>> findAll = service::findAll;
-    return okJson(findAll.asFunction());
+    return okBooks(findAll.asFunction());
   }
 
   public RequestHandler update() {
     Function2<Integer, String, Book> update = service::update;
-    return okJson(update.compose(getBookId(), getBookTitle()));
+    Function1<HttpRequest, Book> compose = update.compose(getBookId(), getBookTitle());
+    return okBook(compose);
   }
 
   public RequestHandler find() {
-    return okJson(getBookId().andThen(service::find));
+    return okBook(getBookId().andThen(service::find));
   }
 
   public RequestHandler create() {
-    return createdJson(getBookTitle().andThen(service::create));
+    return createdBook(getBookTitle().andThen(service::create));
   }
 
   public RequestHandler delete() {
     Consumer1<Integer> delete = service::delete;
-    return okJson(getBookId().andThen(delete.asFunction()));
+    return okBook(getBookId().andThen(delete.asFunction()));
   }
 
   private static Function1<HttpRequest, Integer> getBookId() {
@@ -62,11 +67,19 @@ public class BooksAPI {
     return body().andThen(asString());
   }
 
-  private static <T> RequestHandler okJson(Function1<HttpRequest, T> handler) {
-    return ok(handler.andThen(objectToJson())).andThen(contentJson())::apply;
+  private static <T> RequestHandler okBook(Function1<HttpRequest, T> handler) {
+    Function1<HttpRequest, Try<Bytes>> serialized = handler.andThen(objectToJson(Book.class));
+    return ok(serialized.andThen(Try::get)).andThen(contentJson())::apply;
   }
 
-  private static <T> RequestHandler createdJson(Function1<HttpRequest, T> handler) {
-    return created(handler.andThen(objectToJson())).andThen(contentJson())::apply;
+  private static <T> RequestHandler okBooks(Function1<HttpRequest, T> handler) {
+    Type listOfBooks = new TypeToken<List<Book>>() {}.getType();
+    Function1<HttpRequest, Try<Bytes>> serialized = handler.andThen(objectToJson(listOfBooks));
+    return ok(serialized.andThen(Try::get)).andThen(contentJson())::apply;
+  }
+
+  private static <T> RequestHandler createdBook(Function1<HttpRequest, T> handler) {
+    Function1<HttpRequest, Try<Bytes>> serialized = handler.andThen(objectToJson(Book.class));
+    return created(serialized.andThen(Try::get)).andThen(contentJson())::apply;
   }
 }
